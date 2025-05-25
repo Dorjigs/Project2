8b7a5ac14a0cf71e716400f5696937f257ec58fe
Dorjigs <02230124.cst@rub.edu.bt>
2025-05-26 03:51:00 +0530

    Update dashboard and fix blog APIs

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,19 +1,8 @@
 import { PrismaClient } from '@prisma/client'
 
-let prisma;
-
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient({
-    log: ['error'],
-    errorFormat: 'minimal',
-  });
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient({
-      log: ['query', 'error', 'warn'],
-    });
-  }
-  prisma = global.prisma;
-}
+const prisma = new PrismaClient({
+  log: ['error'],
+  errorFormat: 'minimal',
+});
 
 export default prisma
